f11860b2a0377f46ae8d7b24a21aacb09a4888b3
crocogene <crocogene@yandex.com>
2025-07-30 14:52:52 +0300

    explicit braces

--- src/sched.c
+++ src/sched.c
@@ -92,11 +92,10 @@ sched_add_timer(struct timer *add)
     if (unlikely(timer_is_before(waketime, tl->waketime))) {
         // This timer is before all other scheduled timers
         if (timer_is_before(waketime, timer_read_time()))
-            if (add->too_close_shutdown_reason) {
+            if (add->too_close_shutdown_reason) 
                 sched_try_shutdown(add->too_close_shutdown_reason);
-            } else {
-                try_shutdown("Timer too close");
-            }    
+            else 
+                try_shutdown("Timer too close");    
         if (tl == &deleted_timer)
             add->next = deleted_timer.next;
         else
