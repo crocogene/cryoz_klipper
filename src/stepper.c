d199a69c2817c6c5ff88464fa2fdf967b4b58a59
crocogene <crocogene@yandex.com>
2025-07-30 18:08:46 +0300

    typo

--- src/stepper.c
+++ src/stepper.c
@@ -282,7 +282,7 @@ command_queue_step(uint32_t *args)
         s->flags = flags;
         move_queue_push(&m->node, &s->mq);
         stepper_load_next(s);
-        s->time.too_close_shutdown_reason = TOO_CLOSE_STEPPER
+        s->time.too_close_shutdown_reason = TOO_CLOSE_STEPPER;
         sched_add_timer(&s->time);
     }
     irq_enable();
