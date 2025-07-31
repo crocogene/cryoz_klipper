5e5141b250cc61e6bfe11f5140929423d7396562
crocogene <crocogene@yandex.com>
2025-07-31 23:05:00 +0300

    no timer too close shutdown test

--- src/sched.c
+++ src/sched.c
@@ -96,10 +96,10 @@ sched_add_timer(struct timer *add)
         int32_t time_diff = (int32_t)(waketime - now);
         //if (timer_is_before(waketime, timer_read_time())) {
         if (time_diff < 0) {
-            if (time_diff < -100000) 
-                sched_try_shutdown(add->too_close_shutdown_reason);
-            else
-                waketime = now;                
+            //if (time_diff < -100000) 
+            //    sched_try_shutdown(add->too_close_shutdown_reason);
+            //else
+            waketime = now;                
         } 
         if (tl == &deleted_timer)
             add->next = deleted_timer.next;
